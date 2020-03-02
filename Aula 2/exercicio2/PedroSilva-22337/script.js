b877568aebe2d7c08dd2341278e9749eacf3f8ef
PedroSilva099 <p.e.s@ipvc.pt>
2020-03-02 16:34:22 +0000

    PedroSilva-22337
    ex2

---
+++ Aula 2/exercicio2/PedroSilva-22337/script.js
@@ -0,0 +1,16 @@
+var arrayNumbers = [];
+
+function adicionarNumero() {
+
+    var numero = document.getElementById("numbersId").value;
+    arrayNumbers.push(parseInt(numero));
+    document.getElementById("numerosAdicionados").innerText = arrayNumbers.toString();
+}
+
+function calcularNumero() {
+
+    var maiorNumero = Math.max.apply(Math, arrayNumbers);
+    if(arrayNumbers.length > 5) {
+        alert(maiorNumero);
+    }
+}
